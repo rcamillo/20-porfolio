1c884c48432a396129fe7bbd3dfc0e654909fe08
Lucas Lopes <dev.thuram@gmail.com>
2022-02-10 09:11:21 -0300

    feat: created theme and configure theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  grid: {
+    container: '108rem'
+  },
+  colors: {
+    primary: '#00D2DF',
+    light: '#fff',
+    grayLight: '#C4C4C4',
+    dark: '#121212',
+    darken: '#151515'
+  },
+  gradients: {
+    pink: 'linear-gradient(270.06deg, #6518B4 0%, #D24074 101.62%)',
+    purple: 'linear-gradient(269.96deg, #10D7E2 0%, #9358F7 100%)'
+  },
+  font: {
+    family: 'Inter, sans-serif',
+    sizes: {
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.4rem',
+      xxlarge: '3.2rem',
+      huge: '6.4rem'
+    }
+  },
+  spacings: {
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem'
+  }
+} as const
