06dce64d992991decd25ffe57a4e3ab06c6f42bd
Lucas Lopes <dev.thuram@gmail.com>
2022-02-16 08:36:30 -0300

    feat: create bio component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { FaTwitch, FaYoutube, FaInstagram, FaGithub } from 'react-icons/fa'
 
 import { Picture } from '../Picture'
-import { Wrapper } from './styles'
+
+import { Wrapper, Bio, SocialWrapper } from './styles'
 
 export const About = () => (
   <Wrapper>
@@ -9,6 +11,57 @@ export const About = () => (
       url="https://images.unsplash.com/photo-1624377632657-3902bfd35958?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=100"
       alternativeText="foto de perfil de um programador"
     />
-    <div>sobre mim</div>
+    <Bio>
+      <h2>sobre mim</h2>
+      <span>Belo Horizonte, Brasil</span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
+        egestas dui. Nullam finibus aliquam enim quis faucibus. Aenean ac
+        commodo dolor, nec bibendum velit.
+      </p>
+
+      <SocialWrapper>
+        <li>
+          <a
+            href="https://twitch.tv/devlucaslopes"
+            title="Canal na Twitch"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitch size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/c/devlucaslopes"
+            title="Canal no Youtube"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaYoutube size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/devlucaslopes/"
+            title="Perfil no Instagram"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagram size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/devlucaslopes"
+            title="Perfil no GitHub"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={24} />
+          </a>
+        </li>
+      </SocialWrapper>
+    </Bio>
   </Wrapper>
 )
--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -8,3 +8,50 @@ export const Wrapper = styled.section`
     gap: ${theme.spacings.large};
   `}
 `
+
+export const Bio = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.light};
+
+    h2 {
+      text-transform: uppercase;
+      font-size: ${theme.font.sizes.xlarge};
+      font-weight: 600;
+    }
+
+    span {
+      font-size: ${theme.font.sizes.large};
+      font-weight: 500;
+      margin: ${theme.spacings.large} 0 ${theme.spacings.xsmall} 0;
+      display: block;
+    }
+
+    p {
+      font-size: ${theme.font.sizes.medium};
+      font-weight: 300;
+      color: ${theme.colors.grayLight};
+    }
+  `}
+`
+
+export const SocialWrapper = styled.ul`
+  ${({ theme }) => css`
+    display: flex;
+    gap: ${theme.spacings.medium};
+    margin: ${theme.spacings.large} 0;
+
+    svg {
+      color: ${theme.colors.primary};
+    }
+
+    a > svg {
+      transition: color 300ms ease-in-out;
+    }
+
+    a:hover {
+      svg {
+        color: ${theme.colors.light};
+      }
+    }
+  `}
+`
