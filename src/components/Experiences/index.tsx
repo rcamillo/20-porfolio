f230b3817e8af10fe72c833ae2b3bb82b3e8a304
Lucas Lopes <dev.thuram@gmail.com>
2022-02-18 08:07:57 -0300

    feat: create experience section

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { JobList } from '../JobList'
+import { Subtitle } from '../Subtitle'
 
 import { Wrapper } from './styles'
 
-export const Experiences = () => <Wrapper>xp</Wrapper>
+export const Experiences = () => (
+  <Wrapper>
+    <Subtitle>experiência</Subtitle>
+
+    <JobList />
+  </Wrapper>
+)
