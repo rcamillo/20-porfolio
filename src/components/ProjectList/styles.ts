cd0ad4c073eea265a9cdd3cf46f54268e3dfd97a
Lucas Lopes <dev.thuram@gmail.com>
2022-02-21 09:14:44 -0300

    style: project list responsive

--- src/components/ProjectList/styles.ts
+++ src/components/ProjectList/styles.ts
@@ -1,10 +1,15 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr;
     gap: ${theme.spacings.small};
     margin-top: ${theme.spacings.small};
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: repeat(2, 1fr);
+    `}
   `}
 `
